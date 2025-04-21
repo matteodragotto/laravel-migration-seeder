73069cbe2db6d692ea92f74f48d8414a77add807
matteodragotto <matteodrag8@gmail.com>
2025-04-21 14:39:36 +0200

    [feat] seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,15 +6,30 @@ use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $newTrain = new Train();
 
-        $newTrain->
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+
+            $newTrain->company = $faker->company;
+            $newTrain->departure_station = $faker->city;
+            $newTrain->arrival_station = $faker->city;
+            $newTrain->departure_time = $faker->dateTimeBetween('now', '+1 week')->format('Y-m-d H:i:s');
+            $newTrain->arrival_time = $faker->dateTimeBetween('+1 hour', '+1 week')->format('Y-m-d H:i:s');
+            $newTrain->train_code = $faker->bothify(strtoupper('??####'));
+            $newTrain->train_carriage_number = $faker->randomNumber(2, false);
+            $newTrain->on_time = $faker->boolean(0.5);
+            $newTrain->late = $faker->boolean(0.5);
+
+            $newTrain->save();
+        }
     }
 }
